5c8d33e6d0ee1884af36b6e40010001f592fda4c
Wei Wen <wewen@fb.com>
2018-07-18 17:19:01 -0700

    add beat to wave generator

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 #!/usr/bin/env python
 import re
+from musical.theory import Note, Scale, Chord
+from musical.audio import playback
+
+from timeline import Hit, Timeline
+
+import pygame
+BD = pygame.mixer.Sound('samples/BassDrums1/bassdrum1.wav')
+SD = pygame.mixer.Sound('samples/SnareDrums1/snaredrum1.wav')
+HH = pygame.mixer.Sound('samples/HiHats1/hihat1.wav')
+C1 = pygame.mixer.Sound('samples/Cymbals1/cymbal1.wav')
+C2 = pygame.mixer.Sound('samples/Cymbals1/cymbal2.wav')
+FT = pygame.mixer.Sound('samples/TomTomDrums/tomtomdrum3.wav')
+
+time = 0.0
+timeline = Timeline()
+
+sound_string = {}
+sound_string[BD] = "BD"
+sound_string[SD] = "SD"
+sound_string[HH] = "HH"
+sound_string[C1] = "C1"
+sound_string[C2] = "C2"
+sound_string[FT] = "FT"
+sound_string["BD"] = BD
+sound_string["SD"] = SD
+sound_string["HH"] = HH
+sound_string["C1"] = C1
+sound_string["C2"] = C2
+sound_string["FT"] = FT
+
+def add(t, sound):
+  timeline.add(t, Hit(sound_string[sound], 1, sound))
 
 r = re.compile('(.*)\s\[(\d*)\sx\s(\d*)\sfrom\s\((\d*),\s(\d*)\)\]\n')
+
+BD = pygame.mixer.Sound('samples/BassDrums1/bassdrum1.wav')
+
 
 with open('./example.txt', 'r') as fid:
     lines = fid.readlines()
@@ -16,8 +51,8 @@ for line in lines:
 
 data = filter(lambda x: bool(x), data)
 
-y_thre = 300
-gap_thre = 100
+y_thre = 400
+gap_thre = 200
 beat = []
 for d in data:
     t = d[0][0]
@@ -33,3 +68,17 @@ for d in data:
         beat.append(t)
 
 print(beat)
+
+sound = sound_string["BD"]
+for b in beat:
+    add(b/1000, sound)
+
+print "Rendering audio..."
+
+data = timeline.render()
+
+print "Playing audio..."
+
+playback.play(data)
+
+print "Done!"
